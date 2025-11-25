c870ba59a3c3808abb2fd9a8ce3246243ee2f124
kurrorro <keishavnia@gmail.com>
2025-11-25 14:24:53 +0700

    venue, city, and category models

---
+++ lib/venue/models/city.dart
@@ -0,0 +1,16 @@
+class City {
+    int id;
+    String name;
+
+    City({required this.id, required this.name});
+
+    factory City.fromJson(Map<String, dynamic> json) => City(
+        id: json["id"],
+        name: json["name"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+    };
+}
